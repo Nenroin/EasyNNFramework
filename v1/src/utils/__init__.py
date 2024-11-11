5698ee05809197016c3bab3e125efb12a496ea81
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-11-11 12:58:00 +0300

    create callbacks utils

---
+++ v1/src/utils/__init__.py
@@ -0,0 +1,4 @@
+from v1.src.utils.method_utils import is_overridden
+from v1.src.utils.method_utils import not_overridden
+
+from v1.src.utils.sequential_model_summary_util import print_model_summary
