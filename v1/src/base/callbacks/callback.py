1eea9d2ab038f9e3f4efa186eff52d20061c2c38
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-11-11 19:10:35 +0300

    refactor callbacks

--- v1/src/base/callbacks/callback.py
+++ v1/src/base/callbacks/callback.py
@@ -1,25 +1,21 @@
-from v1.src.base.callbacks.callback_interface import _CallbackInterface
 from v1.src.utils import not_overridden
 
 
-class Callback(_CallbackInterface):
+class Callback:
     def __init__(
             self,
     ):
         self.model = None
-        self.state = {}
+        self.state = None
 
     def set_state(self, state):
         self.state = state
-
-    def update_state(self, state):
-        self.state = self.state.update(state)
 
     def set_model(self, model):
         self.model = model
 
     @not_overridden
-    def on_fit_start(self, epochs, state_objdict=None):
+    def on_fit_start(self, epochs, state_dict=None):
         pass
 
     @not_overridden
@@ -42,12 +38,10 @@ class Callback(_CallbackInterface):
     def on_train_epoch_end(self, epoch, metric_state, state_dict=None):
         pass
 
-    # metric_state - passed from Metric.get_metric_state(self)
     @not_overridden
     def on_test_epoch_start(self, epoch, state_dict=None):
         pass
 
-    # metric_state - passed from Metric.get_metric_state(self)
     @not_overridden
     def on_test_epoch_end(self, epoch, metric_state, state_dict=None):
         pass
@@ -68,26 +62,3 @@ class Callback(_CallbackInterface):
     def on_test_batch_end(self, batch, metric_state, state_dict=None):
         pass
 
-    # @not_overridden
-    # def on_train_forward_start(self, batch_idx, state_dict=None):
-    #     pass
-    # 
-    # @not_overridden
-    # def on_train_forward_end(self, batch_idx, state_dict=None):
-    #     pass
-    # 
-    # @not_overridden
-    # def on_test_forward_start(self, batch_idx, state_dict=None):
-    #     pass
-    # 
-    # @not_overridden
-    # def on_test_forward_end(self, batch_idx, state_dict=None):
-    #     pass
-    # 
-    # @not_overridden
-    # def on_train_backward_start(self, loss_gradient, state_dict=None):
-    #     pass
-    # 
-    # @not_overridden
-    # def on_train_backward_end(self, state_dict=None):
-    #     pass
--- v1/src/base/callbacks/callback_list.py
+++ v1/src/base/callbacks/callback_list.py
@@ -1,140 +1,74 @@
 from v1.src.base.callbacks import Callback
-from v1.src.base.callbacks.callback_interface import _CallbackInterface
 from v1.src.utils import is_overridden
 
 
-class CallbackList(_CallbackInterface):
+class CallbackList(Callback):
     def __init__(
             self,
             model=None,
-            epochs: int = None,
-            train_iterations: int = None,
-            test_iterations: int = None,
             callbacks: [Callback] = None,
-            **kwargs
+            **kwargs,
     ):
-        self.model = model
+        super().__init__()
+        self.set_model(model)
+        self.set_state(kwargs)
         self.__callbacks = callbacks or []
 
-        self.epochs = epochs
-        self.train_iterations = train_iterations
-        self.test_iterations = test_iterations
+    def set_model(self, model):
+        self.model = model
+        for callback in self.__callbacks:
+            callback.set_model(model)
 
-        self.recompile_callbacks(**kwargs)
-
-        self.callbacks_mapping = self.__get_callbacks_mapping()
-
-    def update_params(
-            self,
-            model=None,
-            epochs: int = None,
-            train_iterations: int = None,
-            test_iterations: int = None,
-            **kwargs
-    ):
-        self.model = model or self.model
-
-        self.epochs = epochs or self.epochs
-        self.train_iterations = train_iterations or self.train_iterations
-        self.test_iterations = test_iterations or self.test_iterations
-
-        self.recompile_callbacks(**kwargs)
-
-    def recompile_callbacks(self, **kwargs):
-        [callback.set_model(self.model)
-         for callback in self.__callbacks]
-
-        [callback.update_state(
-            {
-                'epochs': self.epochs,
-                'train_iterations': self.train_iterations,
-                'test_iterations': self.test_iterations,
-                **kwargs,
-            }
-        ) for callback in self.__callbacks]
-
-    def __get_callbacks_mapping(self):
-        callbacks_mapping = {
-            fn_name: self.__extract_functions_from_callbacks(fn_name)
-            for fn_name in _CallbackInterface.get_callback_names()
-        }
-        return callbacks_mapping
-
-    def __extract_functions_from_callbacks(self, function_name):
-        return [getattr(callback, function_name)
-                for callback
-                in self.__callbacks
-                if is_overridden(getattr(callback, function_name))]
+    def set_state(self, state):
+        self.state = state
+        for callback in self.__callbacks:
+            callback.set_state(state)
 
     def on_fit_start(self, epochs, state_dict=None):
-        [callback(epochs, state_dict)
-         for callback in self.callbacks_mapping['on_fit_start']]
+        [callback.on_fit_start(epochs, state_dict)
+         for callback in self.__callbacks]
 
     def on_fit_end(self, state_dict=None):
-        [callback(state_dict)
-         for callback in self.callbacks_mapping['on_fit_end']]
+        [callback.on_fit_end(state_dict)
+         for callback in self.__callbacks]
 
     def on_epoch_start(self, epoch, state_dict=None):
-        [callback(epoch, state_dict)
-         for callback in self.callbacks_mapping['on_epoch_start']]
+        [callback.on_epoch_start(epoch, state_dict)
+         for callback in self.__callbacks]
 
     def on_epoch_end(self, epoch, state_dict=None):
-        [callback(epoch, state_dict)
-         for callback in self.callbacks_mapping['on_epoch_end']]
+        [callback.on_epoch_end(epoch, state_dict)
+         for callback in self.__callbacks]
 
     def on_train_epoch_start(self, epoch, state_dict=None):
-        [callback(epoch, state_dict)
-         for callback in self.callbacks_mapping['on_train_epoch_start']]
+        [callback.on_train_epoch_start(epoch, state_dict)
+         for callback in self.__callbacks]
 
     def on_train_epoch_end(self, epoch, metric_state, state_dict=None):
-        [callback(epoch, metric_state, state_dict)
-         for callback in self.callbacks_mapping['on_train_epoch_end']]
+        [callback.on_train_epoch_end(epoch, metric_state, state_dict)
+         for callback in self.__callbacks]
 
     def on_test_epoch_start(self, epoch, state_dict=None):
-        [callback(epoch, state_dict)
-         for callback in self.callbacks_mapping['on_test_epoch_start']]
+        [callback.on_test_epoch_start(epoch, state_dict)
+         for callback in self.__callbacks]
 
     def on_test_epoch_end(self, epoch, metric_state, state_dict=None):
-        [callback(epoch, metric_state, state_dict)
-         for callback in self.callbacks_mapping['on_test_epoch_end']]
+        [callback.on_test_epoch_end(epoch, metric_state, state_dict)
+         for callback in self.__callbacks]
 
     def on_train_batch_start(self, batch_idx, state_dict=None):
-        [callback(batch_idx, state_dict)
-         for callback in self.callbacks_mapping['on_train_batch_start']]
+        [callback.on_train_batch_start(batch_idx, state_dict)
+         for callback in self.__callbacks]
 
     def on_train_batch_end(self, batch_idx, metric_state, state_dict=None):
-        [callback(batch_idx, metric_state, state_dict)
-         for callback in self.callbacks_mapping['on_train_batch_end']]
+        [callback.on_train_batch_end(batch_idx, metric_state, state_dict)
+         for callback in self.__callbacks]
 
     def on_test_batch_start(self, batch_idx, state_dict=None):
-        [callback(batch_idx, state_dict)
-         for callback in self.callbacks_mapping['on_test_batch_start']]
+        [callback.on_test_batch_start(batch_idx, state_dict)
+         for callback in self.__callbacks]
 
     def on_test_batch_end(self, batch_idx, metric_state, state_dict=None):
-        [callback(batch_idx, metric_state, state_dict)
-         for callback in self.callbacks_mapping['on_test_batch_end']]
+        [callback.on_test_batch_end(batch_idx, metric_state, state_dict)
+         for callback in self.__callbacks]
 
-
-    # def on_train_forward_start(self, batch_idx, state_dict=None):
-    #     [callback(batch_idx, state_dict)
-    #      for callback in self.callbacks_mapping['on_train_forward_start']]
-    #
-    # def on_train_forward_end(self, batch_idx, state_dict=None):
-    #     [callback(batch_idx, state_dict)
-    #      for callback in self.callbacks_mapping['on_train_forward_end']]
-    #
-    # def on_test_forward_start(self, batch_idx, state_dict=None):
-    #     [callback(batch_idx, state_dict)
-    #      for callback in self.callbacks_mapping['on_test_forward_start']]
-    #
-    # def on_test_forward_end(self, batch_idx, state_dict=None):
-    #     [callback(batch_idx, state_dict)
-    #      for callback in self.callbacks_mapping['on_test_forward_end']]
-    #
-    # def on_train_backward_start(self, loss_gradient, state_dict=None):
-    #     [callback(loss_gradient, state_dict)
-    #      for callback in self.callbacks_mapping['on_train_backward_start']]
-    #
-    # def on_train_backward_end(self, state_dict=None):
-    #     [callback(state_dict)
-    #      for callback in self.callbacks_mapping['on_train_backward_end']]
