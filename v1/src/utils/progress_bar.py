819053b02a3c9cad3dd7c9ab877f455d692cf217
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-11-13 14:43:15 +0300

    add progress bar

---
+++ v1/src/utils/progress_bar.py
@@ -0,0 +1,98 @@
+import sys
+import time
+from typing import TextIO
+
+from tqdm import tqdm
+
+
+class ProgressBar:
+    def __init__(
+            self,
+            total: int = None,
+            pb_length: int = 50,
+            fill: chr = '█',
+            out_stream: TextIO = None,
+            prefix: str = '',
+            suffix: str = '',
+            unit: str = 'it',
+            initial: int = 0,
+    ):
+        if out_stream is None:
+            out_stream = sys.stderr
+
+        self.pb_length = pb_length
+        self.fill = fill
+        self.prefix = prefix
+        self.suffix = suffix
+        self.unit = unit
+        self.total = total
+        self.out_stream = out_stream
+        self.initial = initial
+
+        self.iteration = initial
+
+        self.start_time = time.time()
+        self.last_print_time = None
+
+    def update(self, increase=1):
+        self.iteration += increase
+        bar = self.__get_progress_bar()
+        time_section = self.__get_time_section()
+        iterations_section = self.__get_iterations_section()
+        self.display(f'{self.prefix} |{bar}| {iterations_section} {time_section} {self.suffix}')
+
+    def refresh(self):
+        self.update(increase=0)
+
+    def display(self, msg=None):
+        self.last_print_time = self.start_time
+        self.out_stream.write(f'\r{msg}')
+        self.out_stream.flush()
+
+    def __get_progress_bar(self):
+        iteration = min(self.iteration, self.total)
+        filled_length = int(self.pb_length * iteration // self.total)
+        bar = self.fill * filled_length + '-' * (self.pb_length - filled_length)
+        return bar
+
+    def __get_time_section(self):
+        time_to_finish = self.__get_time_to_finish()
+        displayed_time_to_finish = self.__format_time(time_to_finish)
+        passed_time = self.__get_passed_time()
+        displayed_passed_time = self.__format_time(passed_time)
+        return f'[{displayed_passed_time} < {displayed_time_to_finish}]'
+
+    def __get_iterations_section(self):
+        total_digits_number = len(str(self.total))
+        return f'{self.iteration:0{total_digits_number}}{self.unit}/{self.total}{self.unit}'
+
+    def __get_completion_percentage(self):
+        return f'{self.iteration // self.total:.03}%'
+
+    @classmethod
+    def __format_time(cls, seconds):
+        if seconds >= 3600:
+            hours = int(seconds // 3600)
+            minutes = int((seconds % 3600) // 60)
+            formatted_time = f"{hours:02}h{minutes:02}m"
+        elif seconds >= 60:
+            minutes = int(seconds // 60)
+            seconds = int(seconds % 60)
+            formatted_time = f"{minutes:02}m{seconds:02}s"
+        else:
+            milliseconds = int((seconds % 1) * 100)
+            seconds = int(seconds)
+            formatted_time = f"{seconds:02}s{milliseconds:03}ms"
+
+        return formatted_time
+
+    def __get_time_to_finish(self):
+        remaining_iterations = self.total - self.iteration
+        remaining_time = self.__get_average_iteration_duration() * remaining_iterations
+        return remaining_time
+
+    def __get_passed_time(self):
+        return time.time() - self.start_time
+
+    def __get_average_iteration_duration(self):
+        return self.__get_passed_time() / (self.iteration - self.initial)
